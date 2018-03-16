d4c146638657dcad30e16817658c3bb1150d241c
ellen he <ellenhe@ellens-MacBook-Pro.local>
2018-03-16 14:21:22 -0700

    add doctest to merge list

--- interviewcake/merge_sort.py
+++ interviewcake/merge_sort.py
@@ -1,4 +1,16 @@
 def merge_lists(lst1, lst2):
+    """Use merge to sort lists.
+        >>> list1 = [3, 4, 6]
+        >>> list2 = [1, 5]
+        >>> print merge_lists(list1, list2)
+        [1, 3, 4, 5, 6]
+
+        >>> list1     = [3, 4, 6, 10, 11, 12, 15, 20, 20]
+        >>> list2 = [1, 5, 8, 12, 14, 19, 20]
+        >>> print merge_lists(list1, list2)
+        [1, 3, 4, 5, 6, 8, 10, 11, 12, 12, 14, 15, 19, 20, 20, 20]
+
+    """
     result = []
     while lst1 and lst2:
         if lst1[0] < lst2[0]:
@@ -12,7 +24,8 @@ def merge_lists(lst1, lst2):
 
     return result
 
-my_list = [3, 4, 6, 10, 11, 12, 15, 20, 20]
-alices_list = [1, 5, 8, 12, 14, 19, 20]
 
-print merge_lists(my_list, alices_list)
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. NICE STACKING!\n"
